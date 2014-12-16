41c091993ac54ec58801cc48972c9666548999a1
昱铭 <mengyu.songmengyu@alibaba-inc.com>
2014-12-16 23:29:54 +0800

    init client

---
+++ glespClient/src/com/bebetter/glespClient/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bebetter.glespClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SamuraiSong on 16/12/14.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
